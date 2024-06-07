e47e3fbf55712eb4dfe5f5ec9073cdc576d51fbe
renekerr <rrenekerr@gmail.com>
2024-06-07 11:38:43 +0200

    commit from mba15m3

--- 100DaysOfCode/Day_25_CSVs_Data_and_Pandas_Library/squirrel_check.py
+++ 100DaysOfCode/Day_25_CSVs_Data_and_Pandas_Library/squirrel_check.py
@@ -3,9 +3,16 @@ import pandas
 
 # Read data from a CSV file into a DataFrame
 data = pandas.read_csv('Squirrel_Census_20240603.csv')
+gray_ones_count = len(data[data['Primary Fur Color'] == 'Gray'])
+red_ones_count = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+black_ones_count = len(data[data['Primary Fur Color'] == 'Black'])
+print(gray_ones_count)
+print(red_ones_count)
+print(black_ones_count)
 
-# Primary Fur Color
-print(data['Primary Fur Color'])
+data_dict = {
+    'Fur Color': ['Gray', 'Cinnamon', 'Black'],
+    'Count': [gray_ones_count, red_ones_count, black_ones_count]
+}
 
-gray = (data[data['Primary Fur Color'] == 'Gray'])
-print(gray.count())
+print(data_dict)
