a52551ba501b7dfb7350131e3b4de52eb9ba0e63
renekerr <rrenekerr@gmail.com>
2024-10-22 23:05:42 +0200

    pandas df loops iterrows()

---
+++ 100DaysOfCode/Day_26_List_and_Dictionary_Comprehensions/iterate_over_pandas_df.py
@@ -0,0 +1,23 @@
+import pandas
+
+students_score = {
+    "student": ['Alice', 'Bob', 'Charlie', 'Diana', 'Eve'],
+    "score": [90, 67, 88, 100, 99]
+}
+
+print('-Looping through dictionaries')
+for (key, value) in students_score.items():
+    print(key, value)
+
+print('\n-Generating a pandas dataframe')
+students_score_dataframe = pandas.DataFrame(students_score)
+print(students_score_dataframe)
+
+print('\n-Looping through dataframe')
+for (key, value) in students_score_dataframe.items():
+    print(key)
+
+print('\n-Looping through  a dataframe using built-in function')
+for (index, row) in students_score_dataframe.iterrows():
+    if row.score >= 90:
+        print(row.student)  # Students with score higher than or equal to 90
