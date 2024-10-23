4cb2127133bf4704b5cf24e36595de5c28a23abf
renekerr <rrenekerr@gmail.com>
2024-10-23 21:57:18 +0200

    Day 27 GUI with Tkinter and Function Args

---
+++ 100DaysOfCode/Day_27_GUI_Tkinter_Function_Args/main.py
@@ -0,0 +1,10 @@
+# Day 27 - Graphical User Interface with Tkinter and Function Arguments
+import tkinter
+
+window = tkinter.Tk()
+window.title('GUI test')
+window.minsize(width=500, height=500)
+label = tkinter.Label(text='Day 27', font=('Arial', 24, 'bold'))
+label.pack()
+
+window.mainloop()
